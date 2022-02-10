8c750b5301a7680ec89fd03eb4e362ec6fcb2f14
JYGC <jttff1@gmail.com>
2022-02-10 15:28:29 +1100

    commit code

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/bin/env python3
+import random
+import sys
+
+class Element:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+def my_way(home_dict, inbound_arr):
+    inbound_dict = {}
+    for i in inbound_arr:
+        inbound_dict[i.key] = i
+    for k in home_dict:
+        if k in inbound_dict:
+            home_dict[k] = inbound_dict[k].value
+
+def normal_way(home_dict, inbound_arr):
+    for k in home_dict:
+        for i in inbound_arr:
+            if k == i.key:
+                home_dict[k] = i.value
+                inbound_arr.remove(i)
+                break
+
+
+if __name__ == '__main__':
+    max = int(sys.argv[2])
+    home_dict = {random.randint(0, max):None for n in range(0,max)}
+    inbound_arr = [Element(random.randint(0, max), random.randint(0, max)) for n in range(0,max)]
+    op = sys.argv[1]
+    if op == 'a':
+        normal_way(home_dict, inbound_arr)
+    elif op == 'd':
+        my_way(home_dict, inbound_arr)
